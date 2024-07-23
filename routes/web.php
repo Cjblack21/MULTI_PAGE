8399c6dc2057f983487817c0cc5d7b1e9578aa5b
Cjblack <jamesfebriowalker64@gmail.com>
2024-07-23 14:42:14 +0800

    Adding pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+ 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
